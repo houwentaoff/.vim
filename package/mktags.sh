1430c61b6b4352a352e42eb32dcc0305bddb3ff3
Joy Hou <544088192@qq.com>
2019-09-26 23:57:30 +0800

    Add mktag clean

--- package/mktags.sh
+++ package/mktags.sh
@@ -1,10 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 ###############################
 ######Sean: mktags.sh mktags
 ###############################
 PWD=`pwd`
 filename=`basename $PWD`
 cscope_flist=${filename}.files
+
+if [ "$1" == "clean" ]
+then 
+    [ -f $cscope_flist ] && rm $cscope_flist 
+    for f in "cscope.in.out"  "cscope.out"  "cscope.po.out" "tags"
+    do 
+        [ -f $f ] && rm $f
+    done
+    exit 0
+fi
 
 if [ -z $cscope_flist ]
 then
